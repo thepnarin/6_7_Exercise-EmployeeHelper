345b91afe5f66e1d3a153fec47a5fce7dacb7d21
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 23:46:32 +0700

    Exercise-EmployeeHelper

---
+++ 6_7_Exercise-EmployeeHelper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_7_Exercise_EmployeeHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
